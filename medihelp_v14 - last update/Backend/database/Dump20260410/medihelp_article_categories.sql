25d73ebc0aa25e86f42980f082235ad0cdfa0449
B L U E. <167305136+BlueXhima@users.noreply.github.com>
2026-04-10 10:12:26 +0800

    Adding my SQL File

---
+++ medihelp_v14 - last update/Backend/database/Dump20260410/medihelp_article_categories.sql
@@ -0,0 +1,64 @@
+-- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: medihelp
+-- ------------------------------------------------------
+-- Server version	9.6.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ 'e7b57e7e-161f-11f1-bfe1-283a4d879ae0:1-485';
+
+--
+-- Table structure for table `article_categories`
+--
+
+DROP TABLE IF EXISTS `article_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `article_categories` (
+  `category_id` int NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(100) NOT NULL,
+  `icon_name` varchar(50) DEFAULT 'BookOpen',
+  `display_color` varchar(20) DEFAULT 'primary',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`category_id`),
+  UNIQUE KEY `category_name` (`category_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article_categories`
+--
+
+LOCK TABLES `article_categories` WRITE;
+/*!40000 ALTER TABLE `article_categories` DISABLE KEYS */;
+INSERT INTO `article_categories` VALUES (1,'General Health','Heart','primary','2026-04-04 05:42:06'),(2,'Symptoms','Stethoscope','primary','2026-04-04 05:42:06'),(3,'Medications','Pill','primary','2026-04-04 05:42:06'),(4,'Nutrition','Apple','primary','2026-04-04 05:42:06'),(5,'Exercise','Dumbbell','primary','2026-04-04 05:42:06'),(6,'Mental Health','Brain','primary','2026-04-04 05:42:06'),(7,'Chronic Conditions','Activity','primary','2026-04-04 05:42:06'),(8,'Preventive Care','ShieldCheck','primary','2026-04-04 05:42:06'),(9,'Emergency Care','ShieldAlert','primary','2026-04-04 05:42:06'),(10,'Pediatric','Baby','primary','2026-04-04 05:42:06');
+/*!40000 ALTER TABLE `article_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-10 10:10:15
