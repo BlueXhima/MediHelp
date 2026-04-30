4e045b117b92379bfe52ee573fc99c6ec11c476d
B L U E. <167305136+BlueXhima@users.noreply.github.com>
2026-04-30 11:14:48 +0800

    Applying the ClientID in the main file of our project

--- medihelp_v14 - last update/Frontend/src/main.jsx
+++ medihelp_v14 - last update/Frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import axios from 'axios';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId="265618080393-7abaoqhgue70896qi8u8t954ble0i1sb.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>,
 )
