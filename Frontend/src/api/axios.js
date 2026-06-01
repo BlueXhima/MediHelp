923ca53a5d06709c32ac88e7f1c8faf90e0389a2
B L U E. <167305136+BlueXhima@users.noreply.github.com>
2026-06-01 20:50:38 +0800

    Set default API base URL for axios

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -6,7 +6,7 @@ import 'dotenv/config';
 console.log("Process ENV:", process.env.REACT_APP_API_URL);
 
 // Gagamit tayo ng environment variable. 
-const API_BASE_URL = process.env.REACT_APP_API_URL;
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
