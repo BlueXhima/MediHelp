183bb475a637d57700f94bff8a417ed89e5d7eb7
B L U E. <167305136+BlueXhima@users.noreply.github.com>
2026-06-03 14:23:51 +0800

    Switch to VITE_APP_API_URL for environment variable

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 import 'dotenv/config';
 
 // Subukan nating i-log kung ano ang nakikita niya
-console.log("Process ENV:", process.env.REACT_APP_API_URL);
+console.log("Process ENV:", process.env.VITE_APP_API_URL);
 
 // Gagamit tayo ng environment variable. 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = process.env.VITE_APP_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
